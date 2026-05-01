ae6f48d5c92b4773648e91caef5487d6669bfe27
tyos <aihvtd1@gmail.com>
2026-05-01 23:31:52 +0900

    Update runner.py

--- core/runner.py
+++ core/runner.py
@@ -1,10 +1,10 @@
-import asyncio
 import threading
 import uvicorn
 from core.env import Env
 from core.site import app
 from discord_side.client import bot
 import discord_side.commands
+
 
 def web():
     uvicorn.run(app, host=Env.web_host, port=Env.web_port, log_level="warning")
